ee807be5fe259d2cda7cc6f985dc4aba6c184f50
Faizoo <faizaan3424@gmail.com>
2021-10-13 21:18:38 -0400

    fixed id func

--- ok_bot.js
+++ ok_bot.js
@@ -95,7 +95,7 @@ client.on("message", (message) => {
 
     // Aarav spamming:
     const victimChannel = client.channels.cache.get("766654092969771018");
-    const victim = client.users.cache.get("648204656853647371");
+    const victim = client.users.fetch("648204656853647371");
     if (victim.presence.status == "offline") victimChannel.send(victim.id).then(msg => msg.delete(350));
 
 });
