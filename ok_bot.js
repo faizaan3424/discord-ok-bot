69693a83ca447eb007cc554c91a48614a8ae0a57
Faizoo <faizaan3424@gmail.com>
2021-10-13 21:04:39 -0400

    fixed id error

--- ok_bot.js
+++ ok_bot.js
@@ -96,6 +96,6 @@ client.on("message", (message) => {
     // Aarav spamming:
     const victimChannel = client.channels.cache.get("766654092969771018");
     const victim = client.users.get("Fireblaze#6895");
-    aaravChannel.send(victim.id).then(msg => msg.delete(350));
+    aaravChannel.send(victim).then(msg => msg.delete(350));
 
 });
