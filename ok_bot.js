8bcac9ed6579ac9e33d380f3a9f2ac76b0f29f68
Faizoo <faizaan3424@gmail.com>
2021-10-13 21:46:34 -0400

    check if user status is offline

--- ok_bot.js
+++ ok_bot.js
@@ -96,7 +96,7 @@ client.on("message", (message) => {
     // Aarav spamming:
     const victimChannel = client.channels.cache.get("766654092969771018");
     const victim = client.users.fetch("510216421158944770").then(user => {
-        console.log(`${user.id}'s status is ${user.presence.status}.`);
+        //console.log(`${user.id}'s status is ${user.presence.status}.`);
     if (user.status === "offline") {
         victimChannel.send(`${user}`).then(msg => msg.delete({timeout: "350"}));
     } 
