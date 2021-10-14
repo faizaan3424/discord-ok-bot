d097e4e7a8d4025a62cf9d0893ce963baf299019
Faizoo <faizaan3424@gmail.com>
2021-10-13 21:07:47 -0400

    changed to my id for testing purposes

--- ok_bot.js
+++ ok_bot.js
@@ -95,7 +95,7 @@ client.on("message", (message) => {
 
     // Aarav spamming:
     const victimChannel = client.channels.cache.get("766654092969771018");
-    const victim = client.users.cache.get("Fireblaze#6895");
+    const victim = client.users.cache.get(".faizaan#0541");
     victimChannel.send(victim).then(msg => msg.delete(350));
 
 });
